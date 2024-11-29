d57257dbcddc9c371a4bb4d2b0709edbfc757469
Bas van der Heijden <d.s.vanderheijden@tudelft.nl>
2024-11-29 18:32:22 +0100

    uv tests

---
+++ old_tests/test_import.py
@@ -0,0 +1,7 @@
+def test_import():
+    import rex
+
+
+def test_load():
+    from rex.utils import load
+    load("rex.node/Node")
--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,2 @@
 def test_import():
     import rex
-
-
-def test_load():
-    from rex.utils import load
-    load("rex.node/Node")
